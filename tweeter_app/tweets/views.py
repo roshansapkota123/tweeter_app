c8f7f8427864228c5fc2de9384ce1950f9fc7e3c
Roshan <Roshan.sapkota@selu.edu>
2020-11-01 08:57:22 -0600

    app_basics

---
+++ tweeter_app/tweets/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse
+from .models import Tweet
+# Create your views here.
+class TweetListView(ListView):
+    model = Tweet
+    template_name = 'home.html'
+
+class TweetCreateView(CreateView):
+    model = Tweet
+    template_name = 'tweet_new.html'
+    fields = ['user', 'body']
+
+    def get_success_url(self):
+        return reverse('home')
