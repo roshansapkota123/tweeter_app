dc13cac2a20132be6e45b1d534f8312aef7847b5
Roshan <Roshan.sapkota@selu.edu>
2020-11-01 21:45:56 -0600

    added mixins

--- tweeter_app/tweets/views.py
+++ tweeter_app/tweets/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Tweet
 # Create your views here.
 class TweetListView(ListView):
     model = Tweet
     template_name = 'home.html'
 
-class TweetCreateView(CreateView):
+class TweetCreateView(LoginRequiredMixin, CreateView):
     model = Tweet
     template_name = 'tweet_new.html'
-    fields = ['user', 'body']
+    fields = ['body']
 
     def get_success_url(self):
         return reverse('home')
+
+    def get_login_url(self):
+        return reverse('login')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
--- tweeter_app/users/views.py
+++ tweeter_app/users/views.py
@@ -4,17 +4,20 @@ from django.views import generic
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
-class SignUpView(generic.CreateView):
+class SignUpView(SuccessMessageMixin, generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+    success_message = 'Successcully signed you up'
 
-class UserUpdateView(generic.UpdateView):
+class UserUpdateView(SuccessMessageMixin, generic.UpdateView):
     form_class =  CustomUserChangeForm
     success_url = reverse_lazy('home')
     template_name = 'update.html'
+    success_message = 'successfully updated user'
 
     def get_queryset(self):
         user = self.request.user
@@ -23,10 +26,12 @@ class UserUpdateView(generic.UpdateView):
         else:
             return CustomUser.objects.filter(id=user.id)
 
-class UserPasswordChangeView(PasswordChangeView):
+class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView):
     success_url = reverse_lazy('home')
     template_name = 'change_password.html'
+    success_message = 'Sucessfully changed password'
 
-class UserPasswordResetView(PasswordResetView):
+class UserPasswordResetView(SuccessMessageMixin, PasswordResetView):
     success_url = reverse_lazy('login')
     template_name = 'reset_password.html'
+    success_message = 'Please check the email and open the reset link'
